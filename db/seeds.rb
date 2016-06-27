0205715b15d3062c282199576c48514cc5be7925
Nicolas NARDONE <nico.nardone@gmail.com>
2016-06-28 01:56:58 +0200

    added data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create([
+	{name: 'Apiculture'},
+	{name: 'Aquaculture & Pêche'},
+	{name: 'Boissons alcoolisées'}, 
+	{name: 'Boissons non alcoolisées'},
+	{name: 'Elevage'}, 
+	{name: 'Epicerie salée'}, 
+	{name: 'Epicerie sucrée'}, 
+	{name: 'Horticulture'}, 
+	{name: 'Innovation'}, 
+	{name: 'Nature & Environnement'},
+	{name: 'Produits laitiers'},
+	{name: 'Viticulture'}
+])
+
+Project.create([{
+	name: 'Un projet qui pousse, un jardin en devenir', 
+	description: "J'ai besoin d'investir dans du matériel afin d’élargir ma production de fleurs comestibles, jeunes pousses, aromatiques et légumes choisis.",
+	place: 'Rochefort-sur-Loire, France',
+	category_id: 1,
+	amount: 3000
+	},{
+	name: 'Repêchage de la poissonnerie de Carbonne',
+	description: "Participez à la sauvegarde de vos commerces de proximité: la Belle Marine veut rester à Carbonne!",
+	place: 'Carbonne, France',
+	category_id: 2,
+	amount: 1500}
+	]);
